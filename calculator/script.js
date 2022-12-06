1d88e2b01c0b376969b644514b1d884f499e69db
bananapilota <112755624+bananapilota@users.noreply.github.com>
2022-12-06 00:06:20 +0000

    commit

--- calculator/script.js
+++ calculator/script.js
@@ -8,77 +8,77 @@ let buttonClear = document.querySelectorAll('[data-clear]')
 let buttonEqual = document.querySelectorAll('[data-equal]')
 
 
-
-buttonClear.forEach(button =>{
+buttonClear.forEach(button => {
     button.addEventListener('click', () =>{
-        clear()
+        clearAll()
     })
 })
 
 buttonEqual.forEach(button =>{
     button.addEventListener('click', () =>{
-        const sum = previous.innerText + ' ' + display.innerText
-        let split = sum.split(' ')
-        clear()
-        compute(split)
+        compute()
     })
 })
 
 buttonNumber.forEach(button =>{
     button.addEventListener('click', () =>{
-        if(display.innerText === '0'){
-            clearDisplay()
-        }
-        const number = button.innerText
+        number = button.innerText
         appendNumber(number)
     })
 })
 
 buttonOperator.forEach(button =>{
     button.addEventListener('click', () =>{
-        const operator = button.innerText
+        operator = button.innerText
         appendOperator(operator)
+        clearDisplay()
     })
 })
 
-// functions
-
-function appendNumber (number) {
-    if (number === '.' && display.innerText.includes('.')) return
-    display.innerText = display.innerText + number
-}
-
-function appendOperator (operator) {
-    if (display.innerText == '') return
-    display.innerText = display.innerText + ' ' + operator
-    previous.innerText = display.innerText
-    clearDisplay()
-}
-
-function clearDisplay () {
-    display.innerText = ''
-}
-
-function clear () {
+function clearAll(){
     display.innerText = ''
     previous.innerText = ''
 }
 
-function compute (split) {
-    if(split.includes('+')){
-        split = split.map(Number)
-        display.innerText = split[0] + split[2]
+function clearDisplay(){
+    display.innerText = ''
+}
+
+function appendNumber(number){
+    if (number === '.' && display.innerText.includes('.')) return
+    display.innerText = display.innerText + number
+}
+
+function appendOperator(operator){
+    if (display.innerText == '') return
+    if (previous.innerText !== ''){
+        compute()
     }
-    else if(split.includes('-')){
-        split = split.map(Number)
-        display.innerText = split[0] - split[2]
+    previous.innerText = display.innerText
+}
+
+function compute(){
+    let computation
+    const a = parseFloat(previous.innerText)
+    const b = parseFloat(display.innerText)
+    if (isNaN(a) || isNaN(b)) return
+    switch (operator){
+        case '+':
+            computation = a + b
+            break
+        case '-':
+            computation = a - b
+            break
+        case 'x':
+            computation = a * b
+            break
+        case '÷':
+            computation = a + b
+            break
+        default:
+            return
     }
-    else if(split.includes('x')){
-        split = split.map(Number)
-        display.innerText = split[0] * split[2]
-    }
-    else if(split.includes('÷')){
-        split = split.map(Number)
-        display.innerText = split[0] / split[2]
-    }
+    display.innerText = computation
+    operator = undefined
+    previous.innerText = ''
 }
