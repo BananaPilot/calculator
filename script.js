62fc753531f7b29a22485244456c7de329ae1e20
BananaPilot <fabiopetrosillo2@gmail.com>
2022-12-11 17:02:04 +0100

    Co-authored-by: delnero.gabriel@gmail.com <delnero.gabriel@gmail.com>

--- script.js
+++ script.js
@@ -84,8 +84,8 @@ function compute(){
         default:
             return
     }
-    if(computation.toString().length >= 20){
-    display.value = computation.toPrecision(20)
+    if(computation.toString().length >= 15){
+    display.value = computation.toPrecision(15)
     }
     else{
         display.value = computation
